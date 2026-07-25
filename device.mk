28b6b010807373196f4b8acb79561f27a8f82db9
rufnx <naifi195404@gmail.com>
2026-07-25 15:58:59 +0700

    a22x: omni => twrp
    Signed-off-by: rufnx <naifi195404@gmail.com>

--- device.mk
+++ device.mk
@@ -6,3 +6,38 @@
 #
 
 LOCAL_PATH := device/samsung/a22x
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl-recovery \
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-service
+
+# Debug
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+# Sideload
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
+# health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
--- twrp_a22x.mk
+++ twrp_a22x.mk
@@ -10,20 +10,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a22x device
 $(call inherit-product, device/samsung/a22x/device.mk)
 
 PRODUCT_DEVICE := a22x
-PRODUCT_NAME := omni_a22x
+PRODUCT_NAME := twrp_a22x
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A226B
 PRODUCT_MANUFACTURER := samsung
-
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="a22xnsxx-user 12 SP1A.210812.016 A226BXXSBDYA1 release-keys"
-
-BUILD_FINGERPRINT := samsung/a22xnsxx/a22x:12/SP1A.210812.016/A226BXXSBDYA1:user/release-keys
